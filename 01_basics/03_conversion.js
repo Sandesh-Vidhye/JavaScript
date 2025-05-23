eef0133333527ee22a8d00af343168ac5c863c4e
Sandesh Vidhye <158209956+Sandesh-Vidhye@users.noreply.github.com>
2025-05-23 07:34:45 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,22 @@
+// let score = 33
+// console.log(typeof score);
+// let valueInNUmber = Number(score)
+// console.log(typeof valueInNUmber);
+// "33" => 33
+// "33abc" => NaN
+
+
+// let IsLoggedIN = ""
+// let boolearnIsLogIn = Boolean(IsLoggedIN)
+// console.log(boolearnIsLogIn);
+// console.log(IsLoggedIN);
+// 1 =>True , 0 =>False
+//  ""  => False
+//  " " => True
+
+
+let someNumber = 33
+let stringNum = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNum)
+
