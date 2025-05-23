473bbca7fc4887461b569fd47291427a09cda607
Sandesh Vidhye <158209956+Sandesh-Vidhye@users.noreply.github.com>
2025-05-23 05:02:50 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 143
+let accoutEmail = " sandesh@1234.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+
+// accountId = 2
+accountCity ="Rajura"
+accoutEmail = "sandesh1111111111@1234.com"
+accountPassword = "0000000"
+// console.log(accountId);
+// console.log(accountCity)
+console.table([accountId,accountPassword,accoutEmail,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// alert("hello"); We are using Node JS not Browsers
+
+// console.log(3 + 3);
+
+let name = "Sandesh"  //string
+let age = 20          // Number 
+let isLogIn = true    // Boolean
+let state;            //undefined 
+
+console.log(typeof name)
+
