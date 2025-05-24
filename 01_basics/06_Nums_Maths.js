24440af35ca1ce9c0216948b5c4bf1dff81e4079
Sandesh Vidhye <158209956+Sandesh-Vidhye@users.noreply.github.com>
2025-05-24 11:47:59 +0000

    Maths and Num

---
+++ 01_basics/06_Nums_Maths.js
@@ -0,0 +1,33 @@
+const score = 300
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length); // Convert to string
+
+const OtherNUm= 23.56789
+// console.log(OtherNUm.toPrecision(6)); // 23.57
+// console.log(OtherNUm.toFixed(3)); // 23.57
+// console.log(OtherNUm.toExponential(2)); // 2.36e+1
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN')); // 100,000,000
+
+// ===============+++++++++==== Mtahs ++++++++++++++++++++++=========================
+// console.log(Math.abs(-5)); // Mode ke tarah kamkarta hai 
+// console.log(Math.round(5.6)); // 6
+// console.log(Math.ceil(5.1)); // 6
+// console.log(Math.floor(5.9)); // 5
+// console.log(Math.sqrt(300)); // 5
+// console.log(Math.min(5, 10, 2, -1)); // -1
+// console.log(Math.max(5, 10, 2, -1)); // 10
+// console.log(Math.random()); // 0 to 1
+// console.log((Math.random() *10)+1); // 0 to 100
+
+const min = 10 
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
+
+
+
+
+
